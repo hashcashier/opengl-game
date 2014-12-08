ee75eb3d3f4e2c0b77e843ff166a2f80db32e2b1
Rami Khalil <thesavagekoder@gmail.com>
2014-12-08 21:41:01 +0200

    Removed extra file

---
+++ game/GameManager.cpp
@@ -0,0 +1,14 @@
+/*
+ * GameManager.cpp
+ *
+ *  Created on: Dec 8, 2014
+ *      Author: rami
+ */
+
+#include "GameManager.h"
+
+GameManager::GameManager() {
+	// TODO Auto-generated constructor stub
+	GAME_STATE = 0;
+}
+
---
+++ game/GameManager.h
@@ -0,0 +1,17 @@
+/*
+ * GameManager.h
+ *
+ *  Created on: Dec 8, 2014
+ *      Author: rami
+ */
+
+#ifndef GAMEMANAGER_H_
+#define GAMEMANAGER_H_
+
+class GameManager {
+	int GAME_STATE; // 0 = Main Menu, 1 = Paused, 2 = Running, 3 = Over
+public:
+	GameManager();
+};
+
+#endif /* GAMEMANAGER_H_ */
